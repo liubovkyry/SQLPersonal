70389d81a4b8daa36cb618dbc2e2c21a18d3a7a5
Liubov Kyrychenko <118057504+liubovkyry@users.noreply.github.com>
2023-03-26 14:20:17 +0100

    INSERTjson.sql

---
+++ INSERTjson.sql
@@ -0,0 +1,34 @@
+CREATE TABLE emp (  
+  emp_id int NOT NULL,
+  data jsonb
+);
+
+INSERT INTO emp VALUES 
+(1, '{"name": "John", "hobbies": ["Movies", "Football", "Hiking"]}');  
+
+INSERT INTO emp VALUES 
+(2, '{"name": "Marc", "hobbies": ["Gaming", "Movies", "Music"]}');  
+
+
+
+select * from emp;
+
+
+select data->'name' from emp;
+SELECT * FROM emp WHERE data->'name' = '"Marc"'; 
+select data->'hobbies' from emp;
+SELECT data->'hobbies' FROM emp WHERE data->'name' = '"Marc"';
+
+SELECT jsonb_array_elements_text(data->'hobbies') AS hobbies  FROM emp;
+SELECT distinct(jsonb_array_elements_text(data->'hobbies')) AS hobbies  FROM emp;
+
+SELECT emp_id, jsonb_array_elements_text(data->'hobbies') AS hobbies  FROM emp;
+
+SELECT emp_id, data FROM emp WHERE data->'hobbies' @> '["Gaming"]'::jsonb; 
+SELECT emp_id, data FROM emp WHERE data->'hobbies' @> '["Movies"]'::jsonb; 
+
+
+drop table emp;
+
+
+
