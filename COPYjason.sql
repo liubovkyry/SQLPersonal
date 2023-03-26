52cab0fdb2387e9b7eae02adfdd4b9207bb61eeb
Liubov Kyrychenko <118057504+liubovkyry@users.noreply.github.com>
2023-03-26 15:12:01 +0100

    up

--- COPYjason.sql
+++ COPYjason.sql
@@ -27,3 +27,35 @@ from target;
 
 
 select * from covidfeatures
+
+------------
+
+-- rr 
+drop table target
+
+CREATE TEMP TABLE target(v jsonb);
+copy target from 'C:\Users\liubo\Desktop\NewProject\jsonCovid3.json';
+
+
+create table CovidFeatures(business_id varchar(100), delivery_or_takeout varchar(100), highlights varchar(100),
+call_to_action_enabled varchar(100), covid_banner varchar(100),
+grubhub_enabled varchar(100), request_a_quote_enabled varchar(100),
+temporary_closed_until varchar(100), virtual_services_offered varchar(100)
+--, 
+---constraint pk_business_id  primary key (business_id)
+)
+;
+
+--  stg target to ODS  Postgres
+insert into CovidFeatures
+select v->'business_id',
+v->'delivery or takeout',
+v->'highlights',
+v->'Call To Action enabled',
+v->'Covid Banner', v->'Grubhub enabled', v->'Request a Quote Enabled', v->'Temporary Closed Until',
+v->'Virtual Services Offered'
+from target;
+
+truncate table CovidFeatures
+
+select * from covidfeatures
